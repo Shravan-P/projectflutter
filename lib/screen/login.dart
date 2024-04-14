fc30876c7cf91f62950ed1c3d9ade72866087cf8
Shravan-P <shravanpajay@gmail.com>
2024-04-14 15:33:47 +0530

    third push

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -21,91 +21,84 @@ class Screenlogin extends StatelessWidget {
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Image.asset(
-                        'assets/image/4881247.jpg',
-                        height: 150,
+                      Container(
+                        height: 300,
+                        width: 300,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.all(Radius.circular(20)),
+                          child: Image.asset(
+                            'assets/image/White logo.png',
+                            fit: BoxFit.cover,
+                            height: 350,
+                          ),
+                        ),
                       ),
                     ],
                   ),
                   const SizedBox(
                     height: 40,
                   ),
-                  TextFormField(
-                    decoration: const InputDecoration(
-                        border: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.grey),
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(10))),
-                        focusedBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.black),
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(10))),
-                        hintText: 'Username'),
-                  ),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  TextField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.grey),
-                          borderRadius: BorderRadius.all(Radius.circular(10))),
-                      focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.black),
-                          borderRadius: BorderRadius.all(Radius.circular(10))),
-                      hintText: 'Password',
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 10,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Text(
-                        'Dont have an account ?',
-                        style: TextStyle(fontSize: 12),
-                      ),
-                      TextButton(
-                        onPressed: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => Screensignup()));
-                        },
-                        child: Text(
-                          'Sign up',
-                          style: TextStyle(
-                            fontSize: 12,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black,
-                          ),
+                  Container(
+                    width: 300,
+                    child: Column(
+                      children: [
+                        loginindetails('Username', false),
+                        const SizedBox(
+                          height: 15,
                         ),
-                      )
-                      /*Text(
+                        loginindetails('Password', true),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            Text(
+                              'Dont have an account ?',
+                              style: TextStyle(fontSize: 12),
+                            ),
+                            TextButton(
+                              onPressed: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                        builder: (context) => Screensignup()));
+                              },
+                              child: Text(
+                                'Sign up',
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.black,
+                                ),
+                              ),
+                            )
+                            /*Text(
                         'Sign up',
                         style:
                             TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
                       )*/
-                    ],
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(20),
-                    child: ElevatedButton(
-                      onPressed: () {
-                        Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => Screenmainhome()));
-                      },
-                      style: ElevatedButton.styleFrom(
-                          fixedSize: Size(150, 50),
-                          onPrimary: Colors.white,
-                          primary: Colors.black,
-                          textStyle: const TextStyle(
-                              fontSize: 20, fontWeight: FontWeight.bold)),
-                      child: const Text('Login'),
+                          ],
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(20),
+                          child: ElevatedButton(
+                            onPressed: () {
+                              Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => Screenmainhome()));
+                            },
+                            style: ElevatedButton.styleFrom(
+                                fixedSize: Size(150, 50),
+                                onPrimary: Colors.white,
+                                primary: Colors.black,
+                                textStyle: const TextStyle(
+                                    fontSize: 20, fontWeight: FontWeight.bold)),
+                            child: const Text('Login'),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                 ],
@@ -113,6 +106,21 @@ class Screenlogin extends StatelessWidget {
             ),
           ),
         ),
+      ),
+    );
+  }
+
+  TextField loginindetails(String hint, bool obs) {
+    return TextField(
+      obscureText: obs,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.grey),
+            borderRadius: BorderRadius.all(Radius.circular(10))),
+        focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.black),
+            borderRadius: BorderRadius.all(Radius.circular(10))),
+        hintText: hint,
       ),
     );
   }
