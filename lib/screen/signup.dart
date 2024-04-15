c3b158aa8c9565e8317a74aed110c311c88d3881
fahimfiroz <fahim02469@gmail.com>
2024-04-15 10:43:28 +0530

    fixed login pixel issues

--- lib/screen/signup.dart
+++ lib/screen/signup.dart
@@ -96,9 +96,7 @@ class Screensignup extends StatelessWidget {
                           builder: (context) => Screenmainhome()));
                 },
                 style: ElevatedButton.styleFrom(
-                    fixedSize: Size(150, 50),
-                    onPrimary: Colors.white,
-                    primary: Colors.black,
+                    foregroundColor: Colors.white, backgroundColor: Colors.black, fixedSize: Size(150, 50),
                     textStyle: const TextStyle(
                         fontSize: 20, fontWeight: FontWeight.bold)),
                 child: const Text('Create'),
